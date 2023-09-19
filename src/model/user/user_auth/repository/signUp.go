7672dd5d3a13e63061a6c3a974d6a2b11ae8c63a
virussv <teteus.30.08.07@outlook.com>
2023-09-19 17:42:15 -0300

    fixed n+1 query problem in signUp

--- src/model/user/user_auth/repository/signUp.go
+++ src/model/user/user_auth/repository/signUp.go
@@ -2,51 +2,35 @@ package user_auth_repository
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"matheuswww/coffeeShop-golang/src/configuration/logger"
 	"matheuswww/coffeeShop-golang/src/configuration/rest_err"
 	user_auth_model "matheuswww/coffeeShop-golang/src/model/user/user_auth"
 	"time"
 
+	"strings"
+
 	"go.uber.org/zap"
 )
 
 func (ur userAuthRepository) SignUp(userAuthDomain user_auth_model.UserAuthDomainInterface) *rest_err.RestErr {
-	ctx,cancel := context.WithTimeout(context.Background(),5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	logger.Info("Init SignUp repository",zap.String("journey","SignUp"))
+	logger.Info("Init SignUp repository", zap.String("journey", "SignUp"))
 	db := ur.databaseConnection
-	query := "SELECT email from users WHERE email = ?"
-	var existingEmail string
-	rows,err := db.QueryContext(ctx,query,userAuthDomain.GetEmail())
-	if err != nil {
-		if ctx.Err() == context.DeadlineExceeded {
-			logger.Error("Timeout expired",err,zap.String("journey","SignUp"))
-		} else {
-			logger.Error("Error trying exec query",err,zap.String("journey","SignUp"))
-		}
-		return rest_err.NewInternalServerError("database error")
-	}
-	defer rows.Close()
-	if rows.Next() {
-		err = rows.Scan(&existingEmail)
-		if err != nil {
-			logger.Error("Error trying scan query",err,zap.String("journey","SignUp"))
-			return rest_err.NewInternalServerError("database error")
-		}
-		logger.Error("Error trying insert user", errors.New("duplicated email"), zap.String("journey", "SignUp"))
-		return rest_err.NewConflictError("duplicated email")
-	}
-	query = "INSERT INTO users (email,name,password,salt,registration_date,last_access) VALUES (?, ?, ?, ?, ?, ?)"
+	query := `
+		INSERT INTO users (email, name, password, salt, registration_date, last_access)
+		VALUES (?, ?, ?, ?, ?, ?)
+	`
 	location := time.FixedZone("BRT", -3*60*60)
+	fmt.Println(userAuthDomain.GetSalt())
 	timeStamp := time.Now().In(location).Format("2006-01-02 15:04:05")
-	_,err = db.ExecContext(ctx,query,userAuthDomain.GetEmail(),userAuthDomain.GetName(),userAuthDomain.GetEncryptPassword(),userAuthDomain.GetSalt(),timeStamp,timeStamp)
+	_, err := db.ExecContext(ctx, query, userAuthDomain.GetEmail(), userAuthDomain.GetName(), userAuthDomain.GetEncryptedPassword(), userAuthDomain.GetSalt(), timeStamp, timeStamp)
 	if err != nil {
-		logger.Error("Error trying insert user",err,zap.String("journey","SignUp"))
-		return rest_err.NewInternalServerError("database error")
-	}
-	if err != nil {
-		logger.Error("Error trying get id",err,zap.String("journey","SignUp"))
+		logger.Error("Error trying insert user", err, zap.String("journey", "SignUp"))
+		if strings.Contains(err.Error(), "Duplicate entry") {
+			return rest_err.NewBadRequestError("email already exists")
+		}
 		return rest_err.NewInternalServerError("database error")
 	}
 	logger.Info("USER INSERTED IN DATABASE")

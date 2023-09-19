70ac6e1685e62d90afa66d5e9b20fc1f937052b1
virussv <teteus.30.08.07@outlook.com>
2023-09-19 19:03:49 -0300

    created headers middleware

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -3,10 +3,24 @@ package routes
 import (
 	"database/sql"
 	user_routes "matheuswww/coffeeShop-golang/src/controller/routes/user/user_auth_routes"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.RouterGroup,database *sql.DB) {
+	r.Use(Headers)
 	user_routes.InitUserAuthRoutes(r,database)
 }
+
+func Headers(c *gin.Context) {
+	c.Header("X-Permitted-Cross-Domain-Policies", "none")
+	if c.Request.Method == "TRACE" {
+		c.JSON(http.StatusMethodNotAllowed,gin.H{
+			"message": "TRACE method not allowed",
+		})
+		c.Abort()
+		return
+	}
+	c.Next()
+} 
